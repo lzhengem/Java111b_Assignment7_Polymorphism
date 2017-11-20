f7195d7c52553b8de39353fbe93224f6a9d18caf
Lena <lzhengem@gmail.com>
2017-11-19 20:44:41 -0800

    added header for firm

--- Firm.java
+++ Firm.java
@@ -1,18 +1,25 @@
-//********************************************************************
-//  Firm.java       Author: Lewis/Loftus
-//
-//  Demonstrates polymorphism via inheritance.
-//********************************************************************
+/**
+  * Firm.java represents a Firm that has different types of employees. And it will attempt to pay them all
+  *  @author(s) Chien Lin, Lena Zheng, Qd Li
+  *  @version 1.0
+  *  @since 2017-11-19
+*/
 
+/*
+  * Algorithm:
+  * 1. Initialize staff
+  * 2. Set staff's pay
+  * 3. Pay staff
+*/
 public class Firm
 {
    //-----------------------------------------------------------------
    //  Creates a staff of employees for a firm and pays them.
    //-----------------------------------------------------------------
-   public static void main (String[] args)
-   {
-      Staff personnel = new Staff();
+  public static void main (String[] args)
+  {
+	Staff personnel = new Staff();
 
-      personnel.payday();
-   }
+	personnel.payday();
+  }
 }
