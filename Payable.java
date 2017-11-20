419c54af85a50d1f9c7601952e2d018d78ed9e3f
Lena <lzhengem@gmail.com>
2017-11-19 20:15:14 -0800

    kens update

---
+++ Payable.java
@@ -0,0 +1,9 @@
+
+interface Payable
+{
+   //-----------------------------------------------------------------
+   //  Derived classes must define the pay method for each type of
+   //  employee.
+   //-----------------------------------------------------------------
+   public abstract double pay();
+}
